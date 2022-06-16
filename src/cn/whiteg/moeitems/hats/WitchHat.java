6f95c97688d5bf373729e32be8b7607936accb46
MoeWhite19 <MoeWhite19@163.com>
2022-06-17 02:19:39 +0800

    女巫帽生效时有几率扣耐久

--- src/cn/whiteg/moeitems/hats/WitchHat.java
+++ src/cn/whiteg/moeitems/hats/WitchHat.java
@@ -1,6 +1,8 @@
 package cn.whiteg.moeitems.hats;
 
 import cn.whiteg.rpgArmour.api.CustItem_CustModle;
+import cn.whiteg.rpgArmour.utils.ItemToolUtil;
+import cn.whiteg.rpgArmour.utils.RandomUtil;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
@@ -8,6 +10,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
 
 public class WitchHat extends CustItem_CustModle implements Listener {
     private static final WitchHat a = new WitchHat();
@@ -27,14 +30,23 @@ public class WitchHat extends CustItem_CustModle implements Listener {
         return a;
     }
 
-    @EventHandler(ignoreCancelled = true,priority = EventPriority.LOW)
-    public void onDamage(EntityDamageEvent event){
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+    public void onDamage(EntityDamageEvent event) {
         if (event.getCause() == EntityDamageEvent.DamageCause.MAGIC){
-            if(event.getEntity() instanceof LivingEntity livingEntity){
+            if (event.getEntity() instanceof LivingEntity livingEntity){
                 final EntityEquipment equipment = livingEntity.getEquipment();
-                if(equipment != null && is(equipment.getHelmet())){
-//            event.setDamage(EntityDamageEvent.DamageModifier.ARMOR,-(event.getDamage() * 0.85));//修改盔甲值
-                    event.setDamage(event.getDamage() * 0.25); //直接设置总值
+                if (equipment != null){
+                    final ItemStack helmet = equipment.getHelmet();
+                    if (is(helmet)){
+                        final double immune = event.getDamage() * 0.75;
+                        event.setDamage(EntityDamageEvent.DamageModifier.ARMOR,-immune);//修改盔甲值
+                        //扣耐久值
+                        if (ItemToolUtil.damage(helmet,RandomUtil.getRandom().nextInt(2)/*(int) Math.ceil(immune)*/)){
+                            equipment.setHelmet(null);
+//                            livingEntity.getWorld().playSound(Sound.DAMA); //物品损坏播放声音
+                        }
+                        //                    event.setDamage(event.getDamage() * 0.25); //直接设置总值
+                    }
                 }
             }
         }
