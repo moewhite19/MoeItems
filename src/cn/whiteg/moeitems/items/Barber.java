ce694638443cb3f98e5ca9cbc8740f622978f4bb
MoeWhite19 <MoeWhite19@163.com>
2022-07-06 00:07:06 +0800

    理发店只能放一个，不能在一个实体上重复放置,并且可以直接放地上了

--- src/cn/whiteg/moeitems/items/Barber.java
+++ src/cn/whiteg/moeitems/items/Barber.java
@@ -1,5 +1,6 @@
 package cn.whiteg.moeitems.items;
 
+import cn.whiteg.rpgArmour.RPGArmour;
 import cn.whiteg.rpgArmour.api.CustItem_CustModle;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -8,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
@@ -15,17 +17,19 @@ public class Barber extends CustItem_CustModle implements Listener {
 
     public Barber() {
         super(Material.BOWL,160,"理发店");
+        RPGArmour.plugin.getCanBreakEntityItem().addCanPlaceItemFarm(this);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onInteraction(PlayerInteractAtEntityEvent event) {
+        if(event.getHand() != EquipmentSlot.HAND) return;
         final Player player = event.getPlayer();
         final ItemStack hand = player.getItemInHand();
         if (is(hand )){
-            final ItemStack one = hand.asOne();
-            hand.subtract(); //物品减少一个
             Entity clicked = event.getRightClicked();
             if(!clicked.getPassengers().isEmpty())return; //头上有实体时无视
+            final ItemStack one = hand.asOne();
+            hand.subtract(); //物品减少一个
             final Item item = clicked.getWorld().dropItem(clicked.getLocation(),one);
             if (item.isDead()){
                 player.sendActionBar("无法生成掉落物");
@@ -34,6 +38,7 @@ public class Barber extends CustItem_CustModle implements Listener {
             item.setCanMobPickup(false);
             item.setCanPlayerPickup(false);
             clicked.addPassenger(item);
+            event.setCancelled(true);
         }
     }
 
