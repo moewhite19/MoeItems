c0397febae9e7f4e625fa983e649ab0b7ed922dd
MoeWhite19 <MoeWhite19@163.com>
2022-07-05 23:26:37 +0800

    理发店在从头上掉落后依然可以捡起来

--- src/cn/whiteg/moeitems/items/Barber.java
+++ src/cn/whiteg/moeitems/items/Barber.java
@@ -1,19 +1,17 @@
 package cn.whiteg.moeitems.items;
 
 import cn.whiteg.rpgArmour.api.CustItem_CustModle;
-import cn.whiteg.rpgArmour.utils.EntityUtils;
-import net.minecraft.server.level.EntityPlayer;
-import net.minecraft.server.level.WorldServer;
 import org.bukkit.Material;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.spigotmc.event.entity.EntityDismountEvent;
 
 public class Barber extends CustItem_CustModle implements Listener {
-    public static final String TAG = Barber.class.getSimpleName();
 
     public Barber() {
         super(Material.BOWL,160,"理发店");
@@ -23,17 +21,27 @@ public class Barber extends CustItem_CustModle implements Listener {
     public void onInteraction(PlayerInteractAtEntityEvent event) {
         final Player player = event.getPlayer();
         final ItemStack hand = player.getItemInHand();
-        if (is(hand) && event.getRightClicked() instanceof Turtle turtle){
+        if (is(hand )){
             final ItemStack one = hand.asOne();
             hand.subtract(); //物品减少一个
-            final Item item = turtle.getWorld().dropItem(turtle.getLocation(),one);
+            Entity clicked = event.getRightClicked();
+            if(!clicked.getPassengers().isEmpty())return; //头上有实体时无视
+            final Item item = clicked.getWorld().dropItem(clicked.getLocation(),one);
             if (item.isDead()){
                 player.sendActionBar("无法生成掉落物");
                 return;
             }
             item.setCanMobPickup(false);
             item.setCanPlayerPickup(false);
-            turtle.addPassenger(item);
+            clicked.addPassenger(item);
+        }
+    }
+
+    @EventHandler
+    public void onEntityLeave(EntityDismountEvent event){
+        if(event.getEntity() instanceof Item item && is(item.getItemStack())){
+            item.setCanPlayerPickup(true);
+            item.setCanMobPickup(true);
         }
     }
 }
